978452b2eaf880fd7b63bc16b02e27c6f1571970
Anass Mokhless <anass.mokhless123@gmail.com>
2025-12-09 09:17:02 +0100

    oefening4 done

---
+++ oefening4/script.js
@@ -0,0 +1,34 @@
+const multiply = (number1, number2) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(number1 * number2);
+        }, 5000);
+    });
+};
+
+multiply(2, 5)
+    .then((result) => {
+        console.log(result); // 10
+        return multiply(result, 2); // Return the promise
+    })
+    .then((result) => {
+        console.log(result); // 20
+        return multiply(result, 2);
+    })
+    .then((result) => {
+        console.log(result); // 20
+        return multiply(result, 2);
+    })
+    .then((result) => {
+        console.log(result); // 20
+        return multiply(result, 2);
+    })
+    .then((result) => {
+        console.log(result); // 20
+    })
+    .catch((error) => {
+        throw new Error(`Er is een fout opgetreden ${error}`);
+    })
+    .catch((finalerror) => {
+        console.log(finalerror);
+    })
